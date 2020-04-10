f6408e1eba24fd6e46765da49ecef4cacd794971
Zachary Seguin <zachary.seguin@canada.ca>
2020-04-10 16:06:29 -0400

    feat(ns): Add dremio namespace

---
+++ namespace_dremio.tf
@@ -0,0 +1,41 @@
+# DAaaS
+resource "kubernetes_namespace" "dremio" {
+  metadata {
+    name = "dremio"
+
+    # labels = {
+    #   istio-injection = "enabled"
+    # }
+  }
+}
+
+module "namespace_dremio" {
+  source = "git::https://github.com/canada-ca-terraform-modules/terraform-kubernetes-namespace.git"
+
+  name = "${kubernetes_namespace.dremio.metadata.0.name}"
+  namespace_admins = {
+    users  = []
+    groups = var.groups_pachyderm
+  }
+
+  # ServiceAccount
+  helm_service_account = "tiller"
+
+  # ServiceQuota Overrides
+  allowed_loadbalancers = "0"
+  allowed_nodeports     = "0"
+
+  # CICD
+  ci_name = "deploy"
+
+  # Image Pull Secret
+  enable_kubernetes_secret = "${var.enable_kubernetes_secret}"
+  kubernetes_secret        = "${var.kubernetes_secret}"
+  docker_repo              = "${var.docker_repo}"
+  docker_username          = "${var.docker_username}"
+  docker_password          = "${var.docker_password}"
+  docker_email             = "${var.docker_email}"
+  docker_auth              = "${var.docker_auth}"
+
+  dependencies = []
+}
