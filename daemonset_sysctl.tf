96ec588696a3e8636af763dceddf9e530ca4e1b9
Zachary Seguin <zachary.seguin@canada.ca>
2020-04-10 15:01:12 -0400

    feat(kernel): Increase vm.max_map_count for Elasticsaerch

---
+++ daemonset_sysctl.tf
@@ -0,0 +1,92 @@
+resource "kubernetes_daemonset" "sysctl" {
+  metadata {
+    name      = "sysctl"
+    namespace = "kube-system"
+    labels = {
+      "app.kubernetes.io/name" = "sysctl"
+    }
+  }
+
+  spec {
+    strategy {
+      type = "RollingUpdate"
+    }
+
+    selector {
+      match_labels = {
+        "app.kubernetes.io/name" = "sysctl"
+      }
+    }
+
+    template {
+      metadata {
+        annotations = {
+          name = "scheduler.alpha.kubernetes.io/critical-pod"
+        }
+        labels = {
+          "app.kubernetes.io/name" = "sysctl"
+        }
+      }
+
+      spec {
+        container {
+          image = "busybox:latest"
+          name  = "sysctl"
+
+          command = [
+            "/bin/sh",
+            "-c",
+            <<EOF
+            set -o errexit
+            set -o xtrace
+            while sysctl -w vm.max_map_count=262144
+            do
+              sleep 60s
+            done
+            EOF
+          ]
+
+          security_context {
+            privileged = "true"
+          }
+
+          resources {
+            requests {
+              cpu = "10m"
+              memory = "8Mi"
+            }
+
+            limits {
+              cpu = "10m"
+              memory = "8Mi"
+            }
+          }
+        }
+
+        host_pid = true
+        host_ipc = true
+        host_network = true
+        node_selector = {
+          "kubernetes.io/os" = "linux"
+        }
+
+        toleration {
+          key      = "CriticalAddonsOnly"
+          operator = "Exists"
+        }
+
+        toleration {
+          key      = "nvidia.com/gpu"
+          operator = "Exists"
+          effect   = "NoSchedule"
+        }
+
+        toleration {
+          key      = "dedicated"
+          operator = "Exists"
+          effect   = "NoSchedule"
+        }
+      }
+    }
+  }
+}
