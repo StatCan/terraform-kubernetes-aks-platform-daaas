e4a8d1c325c889c4ca6f1c972d989e8f94d7ffdd
Zachary Seguin <zachary.seguin@canada.ca>
2020-04-15 11:14:27 -0400

    feat(minio): Add minio namespace

---
+++ namespace_minio.tf
@@ -0,0 +1,38 @@
+# MinIO
+
+resource "kubernetes_namespace" "minio" {
+  metadata {
+    name = "minio"
+
+    labels = {
+      istio-injection = "enabled"
+    }
+  }
+}
+
+module "namespace_minio" {
+  source = "git::https://github.com/canada-ca-terraform-modules/terraform-kubernetes-namespace.git"
+
+  name = "${kubernetes_namespace.minio.metadata.0.name}"
+  namespace_admins = {
+    users  = []
+    groups = [var.kubernetes_rbac_group]
+  }
+
+  # ServiceAccount
+  helm_service_account = "tiller"
+
+  # CICD
+  ci_name = "deploy"
+
+  # Image Pull Secret
+  # enable_kubernetes_secret = "${var.enable_kubernetes_secret}"
+  # kubernetes_secret = "${var.kubernetes_secret}"
+  # docker_repo = "${var.docker_repo}"
+  # docker_username = "${var.docker_username}"
+  # docker_password = "${var.docker_password}"
+  # docker_email = "${var.docker_email}"
+  # docker_auth = "${var.docker_auth}"
+
+  dependencies = []
+}
