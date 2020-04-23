dadfa312f6b25c0c8af08b172aaed8ffe586c1ba
Zachary Seguin <zachary@zacharyseguin.ca>
2020-04-23 12:34:19 -0400

    feat(minio): Add Open Policy Agent

---
+++ config/minio/policy.rego
@@ -0,0 +1,60 @@
+package httpapi.authz
+import input
+
+default allow = false
+
+rl_permissions := {
+    "user": [{"action": "s3:CreateBucket"},
+             {"action": "s3:DeleteBucket"},
+             {"action": "s3:DeleteObject"},
+             {"action": "s3:GetObject"},
+             {"action": "s3:ListAllMyBuckets"},
+             {"action": "s3:GetBucketObjectLockConfiguration"},
+             {"action": "s3:ListBucket"},
+             {"action": "s3:PutObject"}],
+    "scratch": [{"action": "s3:ListAllMyBuckets"},
+                {"action": "s3:GetObject"},
+                {"action": "s3:ListBucket" }],
+    "admin": [{"action": "admin:ServerTrace"},
+             {"action": "s3:DeleteBucket"},
+             {"action": "s3:DeleteBucket"},
+             {"action": "s3:DeleteObject"},
+             {"action": "s3:GetObject"},
+             {"action": "s3:ListAllMyBuckets"},
+             {"action": "s3:ListBucket"},
+             {"action": "s3:PutObject"}],
+}
+
+allow {
+  input.account == "minioadmin"
+  permissions := rl_permissions["admin"]
+  p := permissions[_]
+  p == {"action": input.action}
+}
+
+allow {
+  username := split(lower(input.claims.preferred_username),"@")[0]
+  input.bucket == username
+  input.claims.organisation_name == "daaas"
+  permissions := rl_permissions["user"]
+  p := permissions[_]
+  p == {"action": input.action}
+}
+
+allow {
+  username := split(lower(input.claims.preferred_username),"@")[0]
+  ref := input.conditions.Referer[_]
+  url := concat("/", ["^http://.*:9000/minio/scratch",username,".*$"] )
+  re_match( url , ref)
+  input.claims.organisation_name == "daaas"
+  permissions := rl_permissions["user"]
+  p := permissions[_]
+  p == {"action": input.action}
+}
+
+allow {
+  input.bucket == "scratch"
+  permissions := rl_permissions["scratch"]
+  p := permissions[_]
+  p == {"action": input.action}
+}
---
+++ helm_opa_minio.tf
@@ -0,0 +1,144 @@
+resource "kubernetes_deployment" "opa_minio" {
+  metadata {
+    name      = "opa"
+    namespace = kubernetes_namespace.minio.metadata.0.name
+    labels = {
+      "app.kubernetes.io/name"     = "opa"
+      "app.kubernetes.io/instance" = "minio"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    strategy {
+      type = "RollingUpdate"
+    }
+
+    selector {
+      match_labels = {
+        "app.kubernetes.io/name"     = "minio"
+        "app.kubernetes.io/instance" = "minio"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          "app.kubernetes.io/name"     = "minio"
+          "app.kubernetes.io/instance" = "minio"
+        }
+      }
+
+      spec {
+        container {
+          image = "openpolicyagent/opa:0.19.1"
+          name  = "opa"
+
+          args = [
+            "run",
+            "--ignore=.*",
+            "--server",
+            "/policies"
+          ]
+
+          port {
+            name           = "http"
+            container_port = 8181
+          }
+
+          volume_mount {
+            name       = "policies"
+            read_only  = true
+            mount_path = "/policies"
+          }
+
+          liveness_probe {
+            http_get {
+              scheme = "HTTP"
+              port   = 8181
+            }
+
+            initial_delay_seconds = 5
+            period_seconds        = 5
+          }
+
+          readiness_probe {
+            http_get {
+              scheme = "HTTP"
+              port   = 8181
+              path   = "/health?bundle=true"
+            }
+
+            initial_delay_seconds = 5
+            period_seconds        = 5
+          }
+
+          resources {
+            requests {
+              cpu    = "10m"
+              memory = "8Mi"
+            }
+
+            limits {
+              cpu    = "10m"
+              memory = "8Mi"
+            }
+          }
+        }
+
+        node_selector = {
+          "kubernetes.io/os" = "linux"
+        }
+
+        volume {
+          name = "policies"
+          config_map {
+            name = "minio-opa-policies"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "opa_minio" {
+  metadata {
+    name      = "opa"
+    namespace = kubernetes_namespace.minio.metadata.0.name
+    labels = {
+      "app.kubernetes.io/name"     = "opa"
+      "app.kubernetes.io/instance" = "minio"
+    }
+  }
+
+  spec {
+    type = "ClusterIP"
+    selector = {
+      "app.kubernetes.io/name"     = "opa"
+      "app.kubernetes.io/instance" = "minio"
+    }
+
+    port {
+      name        = "http"
+      protocol    = "TCP"
+      port        = 8181
+      target_port = 8181
+    }
+  }
+}
+
+resource "kubernetes_config_map" "opa_minio" {
+  metadata {
+    name      = "opa-policies"
+    namespace = kubernetes_namespace.minio.metadata.0.name
+    labels = {
+      "app.kubernetes.io/name"     = "opa"
+      "app.kubernetes.io/instance" = "minio"
+    }
+  }
+
+  data = {
+    "policy.rego" = "${file("${path.module}/config/minio/policy.rego")}"
+  }
+}
